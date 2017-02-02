9d1a5226c7c60e6b032ac9a2894b30194d9592c6
winnie <winnietseng566@gmail.com>
2017-02-03 03:31:16 +0800

    scondtext

--- app/src/main/java/com/tripfortrip/mymap/MainActivity.java
+++ app/src/main/java/com/tripfortrip/mymap/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("Hello");
+        System.out.print("Hi");
+
     }
 }
